8071c1e48b2bd2a35aabf1fdf1cc34d1be73f2f7
Marina Gkioka <gkioka.marina00@gmail.com>
2022-05-18 16:42:03 +0300

    fixed some issues with ex1qA

--- Marina/Ex1/trying.py
+++ Marina/Ex1/trying.py
@@ -1,10 +1,14 @@
 from datetime import datetime, timedelta
-import os
 import pandas as pd
 from day_data import make_day_dataframe
-from year_data import return_stats
 from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
 from merger import *
+import numpy as np
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 
 def additional(): 
     '''Makes additional columns in dataFrame to be used in LSTM model'''
@@ -37,6 +41,7 @@ def additional():
         counter[weekday] += 1
 
         df.loc[df['Date'] == date, 'Energy_at_timestamp'] = dayDf['Sums']
+
         df.loc[df['Date'] == date, 'Day_average'] = avg
         
         date = current_date['Date'] + pd.to_timedelta(1, unit='d')
@@ -49,6 +54,48 @@ def additional():
 def prediction(): 
 
     df = additional()
-    print(df)
+    predictors = df[['Weekday', 'Energy_at_timestamp', 'Weekday_average']]
+    target = df[['Day_average']]
+
+    scaler = MinMaxScaler(feature_range=(0,1))
+
+    X = scaler.fit_transform(predictors)
+    y = scaler.fit_transform(target)
+
+    pred_train, pred_test, tar_train, tar_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+    x_train = []
+    y_train = []
+
+    for i in range(288, len(pred_train)):
+        x_train.append(pred_train[i-288:i, 0:3])
+        y_train.append(tar_train[i, 0])
+
+    x_train, y_train = np.array(x_train), np.array(y_train)
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], x_train.shape[2], 1))
+
+    model = Sequential()
+
+    model = Sequential()
+    model.add(Dense(units=5, input_dim=3, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(units=5, kernel_initializer='normal', activation='tanh'))
+    model.add(Dense(1, kernel_initializer='normal'))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+
+    model.fit(x_train, y_train, batch_size=1, epochs=1)
+
+    x_test = []
+    y_test = target[-(len(tar_test)+1):, 0]
+
+    for i in range(288, len(pred_test)): 
+        x_test.append(pred_test[i-288:i, 0:3])
+
+    x_test = np.array(x_test)
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], x_test.shape[2], 1 ))
+
+
+    predictions = model.predict(x_test)
+    predictions = scaler.inverse_transform(predictions)
+    print(predictions)
 
 prediction()
