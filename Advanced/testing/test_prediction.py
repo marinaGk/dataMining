40b836b2bb919b7a28e84fc2aaab3fd3e1e4a29a
Marina Gkioka <gkioka.marina00@gmail.com>
2022-07-04 22:08:46 +0300

    model problem

--- Advanced/testing/test_prediction.py
+++ Advanced/testing/test_prediction.py
@@ -1,17 +1,27 @@
-from neural_network.withRelu import * 
 import unittest
-import numpy as np 
+import numpy as np
+import sys 
+import os
 
-class TestPrediction: 
+real_path = os.path.realpath(__file__)
+dir_path = os.path.dirname(real_path)
+root_path = os.path.dirname(dir_path)
+
+sys.path.insert(0, root_path)
+from neural_network.withRelu import input_prediction 
+
+class TestPrediction(unittest.TestCase): 
 
     def test_input_prediction(self): 
 
-        demands = [['22216.']['22106.']['22130.']['22040.']['21963.']]
+        demands = [['22216.'],['22106.'],['22130.'],['22040.'],['21963.']]
         demands = np.array(demands)
-        renewables = [['6538.']['6594.']['6630.']['6569.']['6572.']]
+        renewables = [['6538.'],['6594.'],['6630.'],['6569.'],['6572.']]
         renewables = np.array(renewables)
 
-        #15086.36
+        #15086.37
 
         prediction = input_prediction(demands, renewables)
-        
+        prediction = round(prediction, 2)
+
+        self.assertEqual(prediction, 15086.37)
