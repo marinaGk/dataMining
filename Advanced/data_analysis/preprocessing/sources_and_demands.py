99d2dbfa556c45599bab29e6eff49330e643b26b
GiannisKat123 <97844048+GiannisKat123@users.noreply.github.com>
2022-07-05 04:43:41 +0300

    Add files via upload

--- Advanced/data_analysis/preprocessing/sources_and_demands.py
+++ Advanced/data_analysis/preprocessing/sources_and_demands.py
@@ -1,19 +1,22 @@
 import os
 import pandas as pd
 
+directory=os.getcwd()
+os.chdir(directory)
+
 def data_merge(): 
     '''Makes file containing both sources and demands records'''
 
-    real_path = os.path.realpath(__file__) #file path 
-    dir_path = os.path.dirname(real_path) #preprocessing path 
-    dir_path = os.path.dirname(dir_path) #data_analysis path
-    root_path = os.path.dirname(dir_path) #root path
+    real_path = os.path.realpath(__file__)
+    dir_path = os.path.dirname(real_path)
+    dir_path = os.path.dirname(dir_path)
+    root_path = os.path.dirname(dir_path)
 
-    data_path = "{}\data".format(root_path) #works in data
+    data_path = "{}\data".format(root_path)
     os.chdir(data_path) 
 
-    demands_file = data_path + "\\merged_demand_files.csv" #gets file from data
-    sources_file = data_path + "\\merged_source_files.csv" 
+    demands_file = data_path + "\\merged_demand_files.csv"
+    sources_file = data_path + "\\merged_source_files.csv"
 
     demands_df = pd.read_csv(demands_file)
     sources_df = pd.read_csv(sources_file)
@@ -23,6 +26,8 @@ def data_merge():
         column = demands_df[i].tolist()
         sources_df[i] = column
 
-    new_path = data_path + "\\merged_files.csv" #saves in data
+    new_path = data_path + "\\merged_files.csv"
     sources_df.info()
     sources_df.to_csv(new_path, index=False)
+
+# data_merge()
